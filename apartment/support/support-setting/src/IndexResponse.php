42c953610a76a9f8d2041d8a80d446440d5cb87a
DXkite <DXkite@163.com>
2019-03-31 21:20:30 +0800

    添加：默认管理页

--- apartment/support/support-setting/src/IndexResponse.php
+++ apartment/support/support-setting/src/IndexResponse.php
@@ -8,6 +8,19 @@ class IndexResponse extends SignedResponse
 {
     public function onAccessVisit(Request $request)
     {
-        return $this->view('index');
+        if (function_exists('gd_info')) {
+            $gd = gd_info();
+            $gdinfo = $gd['GD Version'];
+        } else {
+            $gdinfo = '不支持';
+        }
+        $upload = ini_get('file_uploads') ? ini_get('upload_max_filesize'):'不支持';
+        return $this->view('index')->set('version', [
+            'suda' => SUDA_VERSION,
+            'php' => PHP_VERSION,
+            'server' => $request->getServer('server-software'),
+            'gd' => $gdinfo,
+          
+        ])->set('upload', $upload);
     }
 }
